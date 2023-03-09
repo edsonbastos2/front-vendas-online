719a209c87840dc5bc0dd625175c197375a16c11
Edson Bastos <abastos@uolin.com>
2023-03-09 17:50:58 -0300

    feat: criado componente de login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import { BackgroundImage } from '../styles/LoginScreen.style';
+
+export const LoginScreen = () => {
+  return (
+    <>
+      <BackgroundImage src="./bg-image.svg" />
+    </>
+  );
+};
