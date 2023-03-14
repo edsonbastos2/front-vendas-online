019b638cee3a77bcd85aae90282540a8afe8fa44
Edson Bastos <abastos@uolin.com>
2023-03-13 21:48:25 -0300

    feat: criado tela login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,15 +1,17 @@
-import { BackgroundImage, Container, Login } from '../styles/LoginScreen.style';
+import { BackgroundImage, Container, LeftSide, RigthSide } from '../styles/LoginScreen.style';
 
 export const LoginScreen = () => {
   return (
     <>
       <Container>
-        <Login>
+        <LeftSide>
           <BackgroundImage src="./bg-image.svg" />
-        </Login>
-        <Login>
-          <input type="text" />
-        </Login>
+        </LeftSide>
+        <RigthSide>
+          <Container>
+            <input type="text" />
+          </Container>
+        </RigthSide>
       </Container>
     </>
   );
--- src/modules/login/styles/LoginScreen.style.ts
+++ src/modules/login/styles/LoginScreen.style.ts
@@ -10,13 +10,25 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex: 1;
-  padding: 0 24px;
+
+  @media (max-width: 576px) {
+    flex-direction: column;
+  }
 `;
 
-export const Login = styled.div`
+export const LeftSide = styled.div`
   width: 100%;
-  flex: 3;
   height: 100vh;
-  padding: 16px 0;
+  padding: 16px;
+
+  @media (max-width: 576px) {
+    display: none;
+  }
+`;
+
+export const RigthSide = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 16px;
+  background-color: red;
 `;
